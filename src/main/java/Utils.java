519cbffbb7daf1041d57b96b7d8ff840f2fa9e59
inkiu <hik0115@gmail.com>
2018-06-17 14:50:54 +0900

    utils class added - java

---
+++ src/main/java/Utils.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Utils {
+    static int[] shuffleArray(int[] ar)
+    {
+        // If running on Java 6 or older, use `new Random()` on RHS here
+        Random rnd = ThreadLocalRandom.current();
+        for (int i = ar.length - 1; i > 0; i--)
+        {
+            int index = rnd.nextInt(i + 1);
+            // Simple swap
+            int a = ar[index];
+            ar[index] = ar[i];
+            ar[i] = a;
+        }
+        return ar;
+    }
+}
