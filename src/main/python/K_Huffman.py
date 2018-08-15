0f1bb83c6e88d5a603474b0801da1fa0b4c9e82d
inkiu <hik0115@gmail.com>
2018-08-15 18:53:36 +0900

    K_Huffman update python

--- src/main/python/K_Huffman.py
+++ src/main/python/K_Huffman.py
@@ -1,6 +1,5 @@
 import sys
 import heapq
-import random
 
 
 def parse_line():
@@ -17,45 +16,32 @@ class Node:
         return self.count < other.count
 
 
-def solution(texts, arity, text_count):
+def short_solution(texts, arity, counts):
     queue = []
     for t in range(texts):
-        child = Node(text_count[t])
-        heapq.heappush(queue, child)
+        heapq.heappush(queue, Node(counts[t]))
 
-    # first_group
     first_group = (len(queue) - arity) % (arity - 1) + 1
-    if first_group != 1:
-        bottom = Node(0)
-        for _ in range(first_group):
-            child = heapq.heappop(queue)
-            bottom.count += child.count
-            bottom.children.append(child)
-        heapq.heappush(queue, bottom)
 
-    # make full tree
     while len(queue) > 1:
         mom = Node(0)
-        for _ in range(arity):
-            if len(queue) != 0:
+        for _ in range(arity if first_group == 1 else first_group):
+            if queue:
+                first_group = 1
                 child = heapq.heappop(queue)
                 mom.count += child.count
                 mom.children.append(child)
         heapq.heappush(queue, mom)
 
-    # use Queue
     ret = 0
-
-    while len(queue) > 0:
-        node = queue.pop()
-        if node is None:
-            continue
-        elif len(node.children) == 0:
-            ret += node.count * node.depth
-        else:
-            for child in node.children:
-                child.depth = node.depth + 1
+    while queue:
+        n = queue.pop()
+        if n.children:
+            for child in n.children:
+                child.depth = n.depth + 1
                 queue.append(child)
+        else:
+            ret += n.count * n.depth
 
     return ret
 
@@ -63,12 +49,4 @@ def solution(texts, arity, text_count):
 cases = parse_line()[0]
 for i in range(cases):
     text, ary = parse_line()
-    print(solution(text, ary, parse_line()))
-
-# for i in range(1000):
-#     text = random.randrange(2, 10000)
-#     ary = random.randrange(2, 10000)
-#     counts = [random.randrange(0, 100000) for _ in range(text)]
-#     print(solution(text, ary, counts))
-
-
+    print(short_solution(text, ary, parse_line()))
